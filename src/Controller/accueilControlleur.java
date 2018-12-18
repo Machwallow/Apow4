1c7446ae783149886a680fef8e97119af6b77573
Stan <stanislasmonnier@hotmail.fr>
2018-12-18 18:03:42 +0100

    creation dossier mvc

--- src/Controller/accueilControlleur.java
+++ src/Controller/accueilControlleur.java
@@ -1,5 +1,28 @@
 package Controller;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
 public class accueilControlleur {
+    public Button buttonOnline;
+    public Button buttonLocal;
+    public Button buttonRules;
+    public Button buttonQuit;
+
+    @FXML
+    private void initialize() {
+        buttonQuit.setOnMouseClicked(event -> {
+                Stage stage = (Stage) buttonQuit.getScene().getWindow();
+                stage.close();
+
+        });
+
+    }
+
+
+
 
 }
