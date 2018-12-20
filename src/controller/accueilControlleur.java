48d346b31952f9a81c4cee2703eb361d2f1e9fc8
Stan <stanislasmonnier@hotmail.fr>
2018-12-20 09:50:51 +0100

    controlleur partie

--- src/controller/accueilControlleur.java
+++ src/controller/accueilControlleur.java
@@ -2,8 +2,14 @@ package controller;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
 public class accueilControlleur {
@@ -11,9 +17,20 @@ public class accueilControlleur {
     public Button buttonLocal;
     public Button buttonRules;
     public Button buttonQuit;
+    public AnchorPane mainPane;
 
     @FXML
     private void initialize() {
+
+        buttonLocal.setOnMouseClicked(event ->{
+            try {
+                AnchorPane pane = FXMLLoader.load(getClass().getResource("../Vue/partieLocal.fxml"));
+                mainPane.getChildren().setAll(pane);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
         buttonQuit.setOnMouseClicked(event -> {
                 Stage stage = (Stage) buttonQuit.getScene().getWindow();
                 stage.close();
